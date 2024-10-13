49faad6f0ff725a12dcdef4f412b8aa13f422b40
Lauane <oliveiralauane91@gmail.com>
2024-10-13 12:20:48 -0300

    feat: add default layout

---
+++ src/layout/DefaultLayout/index.tsx
@@ -0,0 +1,14 @@
+// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Header from "../../components/Header";
+
+export function DefaultLayout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow bg-gray-300 p-4">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
