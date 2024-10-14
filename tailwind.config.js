feac36de9bc7fda295e1e677b216995935a80129
Lauane <oliveiralauane91@gmail.com>
2024-10-14 10:18:35 -0300

    style: config default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // Inclui arquivos .jsx e .tsx
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
