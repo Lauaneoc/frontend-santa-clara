4151618980c1dc4e7a70a376a292820408e0f804
Lauane <oliveiralauane91@gmail.com>
2024-10-14 10:18:47 -0300

    feat: add Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,14 @@
-type ButtonProps = {
-    placeholder?: string
+interface ButtonProps {
+    children?: React.ReactNode
 }
 
-export function Button({placeholder = 'you@example.com', ...props}:ButtonProps) {
+export function Button({children}:ButtonProps) {
     return (
-        <div>
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-            Email
-            </label>
-            <div className="mt-2">
-                <input
-                    {...props}
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder={placeholder}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-            </div>
-        </div>
+        <button
+            type="button"
+            className="rounded-md box-border font-inter h-fit bg-[#8B1513] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+            {children}
+      </button>
     )
 }
