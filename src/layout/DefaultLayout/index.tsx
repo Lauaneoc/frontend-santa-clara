92a140a23df570b9dd8a4027ecebffefdb553010
Lauane <oliveiralauane91@gmail.com>
2024-10-14 10:20:17 -0300

    style: add font inter

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -4,7 +4,7 @@ import Header from "../../components/Header";
 
 export function DefaultLayout() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen  font-inter">
       <Header />
       <div className="flex-grow bg-gray-300 p-4">
         <Outlet />
